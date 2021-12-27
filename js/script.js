abe3df61a8d3e9226dde91050770c0dba1f1a16b
ahsan-emon <ahsan.emon23@gmail.com>
2021-12-27 20:52:02 +0600

    our work + facebook analysis form + get in touch + footer part completed
    html5 & css3 used to complete those part

---
+++ js/script.js
@@ -0,0 +1,7 @@
+// (function($){
+//     $(document).ready(function(){
+//         $('.fa-chevron-circle-down').focus(function(){
+//             $('.form-box').hide();
+//         });
+//     });
+//   })(jQuery)
